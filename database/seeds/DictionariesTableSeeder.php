ed2e8af1f431207d5bf26f75ccee1a22900ee930
kay_yu <kay71029@gmail.com>
2017-03-20 10:42:04 +0800

    add migrations/seeder/models

---
+++ database/seeds/DictionariesTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DictionariesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        date_default_timezone_set('Asia/Taipei');
+        DB::table('dictionaries')->truncate();
+        $faker = Faker\Factory::create();
+        $faker2 = Faker\Factory::create('zh_CN');
+        $faker3 = Faker\Factory::create('zh_TW');
+        $limit = 100;
+        //英文假資料
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('dictionaries')->insert([ //,
+                'lang' => 'en',
+                'word' => $faker->unique()->word,
+                'founder' =>'user1',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
+            ]);
+        }
+        //中文假資料_zh_CN
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('dictionaries')->insert([ //,
+                'lang' => 'zh_CN',
+                'word' => $faker2->unique()->name,
+                'founder' =>'user2',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
+            ]);
+        }
+        //中文假資料_zh_TW
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('dictionaries')->insert([ //,
+                'lang' => 'zh_TW',
+                'word' => $faker3->unique()->name,
+                'founder' =>'user3',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
+            ]);
+        }
+    }
+}
