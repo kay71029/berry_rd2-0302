698d4494ed748bab1adf535e6a3b08a488737375
kay_yu <kay71029@gmail.com>
2017-03-20 13:32:30 +0800

    addlanguageseeder

---
+++ database/seeds/LanguagesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LanguagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        date_default_timezone_set('Asia/Taipei');
+        DB::table('languages')->truncate();
+
+        DB::table('languages')->insert([
+            'lang' => 'zh_CN',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ]);
+
+        DB::table('languages')->insert([
+            'lang' => 'zh_TW',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ]);
+
+        DB::table('languages')->insert([
+            'lang' => 'en_US',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ]);
+    }
+
+}
