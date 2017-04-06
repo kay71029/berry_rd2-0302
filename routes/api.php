99801efc8bb2013cc7d5dbd9cc67d0b3e6c41f81
kay_yu <kay71029@gmail.com>
2017-04-06 17:47:44 +0800

    api定義

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+//新增語系
+
+//修改語系
+
+//查詢語系
+
+//刪除語系
+
+//新增詞彙
+
+//修改詞彙
+
+//查詢詞彙
+
+//刪除詞彙
